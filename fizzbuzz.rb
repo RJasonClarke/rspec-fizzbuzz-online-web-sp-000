a8eff461d81e27fc1b322173e92467a046b1d2f3
Robert Clarke <r.jason.clarke@gmail.com>
2020-12-23 03:30:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,20 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './spec_helper.rb'
-
 def fizzbuzz(int)
-  if (int % 3 == 0) && (int % 5 == 0)
-    "Fizzbuzz"
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 == 0
     "Fizz"
-  else
-    nil
+  else nil
   end
 end
-
-
-return fizzbuzz(3)
-return fizzbuzz(5)
-return fizzbuzz(4)
